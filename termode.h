cebd48501dd2d43befe61058d3418deaace5848c
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2024-03-17 22:34:58 -0300

    echo on/off

--- termode.h
+++ termode.h
@@ -4,13 +4,15 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <unistd.h>
-#include <stdio.h>
 
 #define NONBLOCKING_OFF 0
 #define NONBLOCKING_ON 1
 
 #define CANONICAL_OFF 0
 #define CANONICAL_ON 1
+
+#define ECHO_OFF 0
+#define ECHO_ON 1
 
 struct termios new_term, old_term;
 
@@ -41,6 +43,22 @@ void termode_canonical(int mode) {
         new_term.c_cc[VMIN] = 0;
         new_term.c_cc[VTIME] = 0;
 
+        tcsetattr(STDIN_FILENO, TCSANOW, &new_term);
+    }
+}
+
+void termode_echo(int mode) {
+    if(mode != ECHO_OFF && mode != ECHO_ON) return;
+
+    if(mode == ECHO_OFF) {
+        tcgetattr(STDIN_FILENO, &new_term);
+        new_term.c_lflag &= ~ECHO;
+        tcsetattr(STDIN_FILENO, TCSANOW, &new_term);
+    }
+
+    if(mode == ECHO_ON) {
+        tcgetattr(STDIN_FILENO, &new_term);
+        new_term.c_lflag |= ECHO;
         tcsetattr(STDIN_FILENO, TCSANOW, &new_term);
     }
 }
